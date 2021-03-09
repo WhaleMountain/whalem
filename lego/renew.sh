cccf073d25e01beafab33d81aa7bcd7c55cd8ee7
Whale Mountain <whalem@YoRHa.local>
2021-03-09 13:47:23 +0900

    init

---
+++ lego/renew.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+DOMAINS=$1
+
+if [ -z $DOMAINS ]; then
+	echo "Please domain"
+	exit 1
+fi
+
+EMAIL="whalemountain4@gmail.com"
+CMD="--email $EMAIL --domains $DOMAINS --http renew"
+
+docker run \
+	-v /home/mk/whalemountain/lego/certs:/.lego \
+	-p 80:80 \
+	-p 443:443 \
+	--rm -it \
+	--name lego \
+	goacme/lego:v4.1.3 $CMD
